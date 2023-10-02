15dfe69c518dd8914c2360316d1a0df3aad9ed2b
artem <ki4igin1992@outlook.com>
2023-10-02 23:32:09 +0300

    add velocity

--- code/Src/motor.h
+++ code/Src/motor.h
@@ -15,8 +15,16 @@ struct motor {
     int32_t pos_step;
     int32_t offset_step;
     uint32_t k_mov2step;
-    TIM_TypeDef *tim_inst;
-    volatile uint32_t *const ccr;
+
+    struct {
+        TIM_TypeDef *inst;
+        volatile uint32_t *const ccr;
+    } tim;
+
+    struct {
+        uint16_t req_change;
+        uint16_t value;
+    } velocity;
 };
 
 inline static void motor_offset(struct motor *s, int32_t offset_mm)
@@ -29,7 +37,7 @@ inline static void motor_offset(struct motor *s, int32_t offset_mm)
     } else {
         LL_GPIO_ResetOutputPin(s->gpio_dir.port, s->gpio_dir.pin);
     }
-    LL_TIM_EnableCounter(s->tim_inst);
+    LL_TIM_EnableCounter(s->tim.inst);
 }
 
 inline static int32_t motor_get_pos(struct motor *s)
@@ -39,15 +47,17 @@ inline static int32_t motor_get_pos(struct motor *s)
 
 inline static void motor_set_velocity(struct motor *s, uint32_t vel_papugay)
 {
-    if (LL_TIM_IsEnabledCounter(s->tim_inst)) {
-        return;
-    }
     if (vel_papugay < 1000) {
         vel_papugay = 1000;
     }
+    if (LL_TIM_IsEnabledCounter(s->tim.inst)) {
+        s->velocity.req_change = 1;
+        s->velocity.value = vel_papugay;
+        return;
+    }
 
-    *s->ccr = vel_papugay / 2;
-    LL_TIM_SetAutoReload(s->tim_inst, vel_papugay);
+    *s->tim.ccr = vel_papugay / 2;
+    LL_TIM_SetAutoReload(s->tim.inst, vel_papugay);
 }
 
 inline static void motor_set_origin(struct motor *s)
@@ -57,6 +67,12 @@ inline static void motor_set_origin(struct motor *s)
 
 inline static void motor_mov_process(struct motor *s)
 {
+    if (s->velocity.req_change) {
+        s->velocity.req_change = 0;
+        *s->tim.ccr = s->velocity.value / 2;
+        LL_TIM_SetAutoReload(s->tim.inst, s->velocity.value);
+    }
+
     if (s->offset_step > 0) {
         LL_GPIO_SetOutputPin(s->gpio_dir.port, s->gpio_dir.pin);
         s->offset_step--;
@@ -66,7 +82,7 @@ inline static void motor_mov_process(struct motor *s)
         s->offset_step++;
         s->pos_step--;
     } else {
-        LL_TIM_DisableCounter(s->tim_inst);
+        LL_TIM_DisableCounter(s->tim.inst);
     }
 }
 
